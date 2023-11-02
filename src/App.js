f83af3c3bfdda6b29d0523a7a6590e453138feb2
Precious-gift <falayeprecious@gmail.com>
2023-11-02 08:35:01 +0100

    new commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import "./App.css";
+import categoriesList from "./components/category-list/category-list";
+import Directory from "./components/directory/directory.component";
 
 const App = () => {
-  return <div>Hello World</div>;
+  return <Directory categories={categoriesList} />;
 };
 
 export default App;
