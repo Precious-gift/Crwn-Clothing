5e89fc061524ef77b0466c97074bc8c02351d7fa
Precious-gift <falayeprecious@gmail.com>
2023-11-07 21:59:51 +0100

    Sign in with google popup

--- src/components/routes/sign-in/sign-in.component.jsx
+++ src/components/routes/sign-in/sign-in.component.jsx
@@ -1,7 +1,15 @@
+import { signInWithGooglePopUp } from "../../../utils/firebase/firebase.utils";
+
 const SignIn = () => {
+  const logGoogleUser = async () => {
+    const response = await signInWithGooglePopUp();
+    console.log(response);
+  };
+
   return (
     <div>
       <h1>This is the sign in page</h1>
+      <button onClick={logGoogleUser}>Sign In With Google Popup</button>
     </div>
   );
 };
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,4 +1,10 @@
 import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -15,3 +21,11 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+  prompt: "select_account",
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopUp = () => signInWithPopup(auth, provider);
